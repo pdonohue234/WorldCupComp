721b7e1a7bddad94a740318c2df1639bc118c1a3
pdonohue <pdonohue@PDONOHUE-MOBL4.ger.corp.intel.com>
2014-05-27 22:43:11 +0100

    Layout of predictions table. Done in index and needs to be incorporated into predictions.jsp New JS file for paging table

--- WorldCupComp/WorldCupComp-war/src/main/webapp/js/functions.js
+++ WorldCupComp/WorldCupComp-war/src/main/webapp/js/functions.js
@@ -180,3 +180,11 @@ function supports_html5_storage() {
 		return false;
 	}
 }
+
+function toggle_visibility(id) {
+    var e = document.getElementById(id);
+    if(e.style.display == 'block')
+       e.style.display = 'none';
+    else
+       e.style.display = 'block';
+ }
